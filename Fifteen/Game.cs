b65c538520253cc647bd665a4941e5ef320baee8
Maryoku <maryoku.amaya@gmail.com>
2017-02-20 04:11:47 +0500

    add draft solution

---
+++ Fifteen/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifteen
+{
+    class Game
+    {
+        public int[,] field;
+
+        public Game(params int[] values)
+        {
+            int size = (int)Math.Sqrt(values.Length);
+
+            this.field = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    field[i, j] = values[i*size+j];
+                }
+            }
+            
+        }
+
+        int this[int i, int j]
+        {
+            get
+            {
+                return field[i, j];
+            }
+        }
+
+        public void GetLocation(int value, out int x, out int y)
+        {
+            int size = (int)Math.Sqrt(field.Length);
+            x = 0;
+            y = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (field[i, j] == value)
+                    {
+                        x = i;
+                        y = j;
+
+                        Console.WriteLine("{0} {1}", i, j);
+                    }
+                }
+            }
+
+        }
+
+        public int Shift
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Fifteen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game fifteen = new Game(1, 2, 3, 4, 5, 6, 7, 8, 0);
+
+            int x, y;
+            fifteen.GetLocation(2, out x, out y);
+        }
+    }
+}
