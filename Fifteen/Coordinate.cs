cb519c479bf0dadd517bf9cb58e1b5f5b038c646
Maryoku <maryoku.amaya@gmail.com>
2017-03-01 21:47:17 +0500

    add new solution

---
+++ Fifteen/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifteen
+{
+    class Coordinate
+    {
+        public int X;
+        public int Y;
+
+        public Coordinate(int i, int j)
+        {
+            this.X = i;
+            this.Y = j;
+        }
+
+        public static double operator -(Coordinate a, Coordinate b)
+        {
+            return Math.Sqrt(Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.X - b.X, 2));
+        }
+    }
+}
--- Fifteen/Game.cs
+++ Fifteen/Game.cs
@@ -3,130 +3,144 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Fifteen
 {
     class Game
     {
-        public int[,] field;
+        public int[,] Field;
+        private int zeroX;
+        private int zeroY;
+
+        public Dictionary<int, Coordinate> Dictionary = new Dictionary<int, Coordinate>();
 
         public Game(params int[] values)
         {
+            bool zero = false;
+            int count = 0;
+
             int size = (int)Math.Sqrt(values.Length);
 
-            this.field = new int[size, size];
+            if (values.Length != Math.Pow(size, 2) || (values.Length % 2 == 0))
+            {
+                throw new ArgumentException("Поле не соответстует правилам");
+            }
+
+            if (!CheckUnique(values))
+            {
+                throw new ArgumentException("Значения не уникальны");
+            }
+
+            if (CheckNegative(values))
+            {
+                throw new ArgumentException("Отрицательное число");
+            }
+
+            this.Field = new int[size, size];
 
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
-                    field[i, j] = values[i*size+j];
+                    if (values[count] == 0)
+                    {
+                        zero = true;
+                        zeroX = i;
+                        zeroY = j;
+                        Field[i, j] = values[count];
+                        Dictionary.Add(count, new Coordinate(i, j));
+                        count++;
+                    }
+                    else
+                    {
+                        Field[i, j] = values[count];
+                        Dictionary.Add(count, new Coordinate(i, j));
+                        count++;
+                    }
+
                 }
             }
-            
+
+            if (!zero) throw new ArgumentException("Отсутствует пустое  поле");
         }
 
-        int this[int i, int j]
+        public int this[int i, int j]
         {
             get
             {
-                return field[i, j];
+                return Field[i, j];
+            }
+            set
+            {
+                Field[i, j] = value;
             }
         }
 
-        public void GetLocation(int value, out int x, out int y)
+        private Coordinate GetLocation(int value)
         {
-            int size = (int)Math.Sqrt(field.Length);
-            x = 0;
-            y = 0;
-
-            for (int i = 0; i < size; i++)
+            if ((value < Math.Pow(value, 2) - 1) && (value > 0))
             {
-                for (int j = 0; j < size; j++)
-                {
-                    if (field[i, j] == value)
-                    {
-                        x = i;
-                        y = j;
-
-                        //Console.WriteLine("{0} {1}", i, j);
-                    }
-                }
+                return Dictionary[value];
             }
-
+            else throw new ArgumentException("Числа " + value + " не существует");
         }
 
         public void Shift(int value)
         {
-            int size = (int)Math.Sqrt(field.Length);
-            int tmp, i, j;
-
-            GetLocation(value, out i, out j);
-
-            if ((j+1 <= size - 1) && (field[i, j+1] == 0))
+            if (Dictionary[value] - Dictionary[0] == 1)
             {
-                tmp = field[i, j];
-                field[i, j] = field[i, j + 1];
-                field[i, j + 1] = tmp;
-
-                for (int m = 0; m < size; m++)
-                {
-                    for (int n = 0; n < size; n++)
-                    {
-                        Console.Write("{0} ", field[m, n]);
-                    }
-                    Console.Write("\n");
-                }
-            }
-            else if ((j-1 >= 0) && (field[i, j-1] == 0))
-            {
-                tmp = field[i, j];
-                field[i, j] = field[i, j - 1];
-                field[i, j - 1] = tmp;
-
-                for (int m = 0; m < size; m++)
-                {
-                    for (int n = 0; n < size; n++)
-                    {
-                        Console.Write("{0} ", field[m, n]);
-                    }
-                    Console.Write("\n");
-                }
-            }
-            else if ((i+1 <= size-1) && (field[i+1, j] == 0))
-            {
-                tmp = field[i, j];
-                field[i, j] = field[i + 1, j];
-                field[i + 1, j] = tmp;
-
-                for (int m = 0; m < size; m++)
-                {
-                    for (int n = 0; n < size; n++)
-                    {
-                        Console.Write("{0} ", field[m, n]);
-                    }
-                    Console.Write("\n");
-                }
-            }
-            else if ((i-1 >= 0) && (field[i-1, j] == 0))
-            {
-                tmp = field[i, j];
-                field[i, j] = field[i-1, j];
-                field[i-1, j] = tmp;
-
-                for (int m = 0; m < size; m++)
-                {
-                    for (int n = 0; n < size; n++)
-                    {
-                        Console.Write("{0} ", field[m, n]);
-                    }
-                    Console.Write("\n");
-                }
+                Coordinate positionZero = Dictionary[0];
+                this[Dictionary[0].X, Dictionary[0].Y] = value;
+                this[Dictionary[value].X, Dictionary[value].Y] = 0;
+                Dictionary[0] = Dictionary[value];
+                Dictionary[value] = positionZero;
             }
             else
             {
-                Console.WriteLine("Can't move!");
+                throw new ArgumentException("Невозможно передвинуть фишку");
             }
         }
+
+        private bool CheckUnique(int[] mass)
+        {
+            int temp;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                temp = mass[i];
+                for (int j = i + 1; j < mass.Length; j++)
+                {
+                    if (mass[j] == temp)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool CheckNegative(int[] values)
+        {
+            foreach (int i in values)
+            {
+                return values[i] < 0;
+            }
+            return true;
+        }
+
+        public static Game ReadCSV(string file)
+        {
+            string[] Lines = File.ReadAllLines(file);
+            int[] elem = new int[Lines.Length];
+
+            for (int i = 0; i < Lines.Length; i++)
+            {
+                string s = Lines[i];
+                string[] substring = s.Split(';');
+                elem[i] = Convert.ToInt32(substring[i]);
+            }
+            return new Game(elem);
+        }
     }
+
 }
